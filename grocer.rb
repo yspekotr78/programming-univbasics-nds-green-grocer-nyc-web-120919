9c9a390713734601ba5cae8678b4a71756053a9e
Yury Spektor <yspektor78@gmail.com>
2019-11-30 17:21:07 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -118,7 +118,7 @@ def checkout(cart, coupons)
   checkout = apply_clearance(checkout)
   index = 0
     while index < checkout.length do
-      current_item = cart[index]
+      current_item_total = cart[index]
       total += (current_item[:price] * current_item[:count])
       total = total.round(2)
       index +=1
